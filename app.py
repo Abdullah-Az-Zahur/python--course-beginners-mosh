c5d3c0df29005d3f6e5e480bb42ade5ad51e0efc
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2026-03-16 15:15:34 +0600

    elif

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-temperature = 35
+temperature = 10
 
 if temperature > 30:
     print("water is hot")
+    print("drink water")
+elif temperature > 20:
+    print("its nice")
+else:
+    print("its cold")
 
 print("done execution")
