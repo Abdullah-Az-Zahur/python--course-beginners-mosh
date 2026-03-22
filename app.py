f5989df7bbea785ed71ce6004fb81b17fbce3eb7
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2026-03-22 14:57:40 +0600

    function

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-count = 0
+def greet():
+    print("Hi there")
+    print("Welcome to abroad")
 
-for number in range(1, 10):
-    if number % 2 == 0:
-        count = count + 1
-        print(number)
 
-print(f"we have {count} even number")
+greet()
